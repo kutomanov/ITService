3d78c2a9d2b60486a36670c20d03e5316981ad18
kutomanov <kutomanov@mail.ru>
2017-04-16 21:56:42 +0400

    Add files via upload

---
+++ ITServive/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace ITServive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 147);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(18, 266);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(365, 29);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(94, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(185, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Загрузить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(276, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "(*.txt)|*.txt";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\Евгения\\Desktop\\ИТ-Сервис";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.InitialDirectory = "C:\\Users\\Евгения\\Desktop\\ИТ-Сервис";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Условие";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(20, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Результат";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(106, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(169, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Задание 1",
+            "Задание 2"});
+            this.comboBox1.Location = new System.Drawing.Point(20, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(101, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 215);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(282, 35);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(121, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(20, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 15);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Выбор задания";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(308, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Очистить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(308, 227);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(71, 23);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Очистить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(387, 302);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Приложение ИТ-Сервис";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ITServive/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace ITServive
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        private int number;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {                
+                name = openFileDialog1.FileName;
+                comboBox1.Text = "";
+                textBox1.Clear();
+                textBox2.Clear();
+                comboBox1.Text = File.ReadLines(name).First();
+                if (File.ReadLines(name).First().Equals("Задание 2"))
+                {
+                    textBox1.Text = File.ReadLines(name).Skip(2).First();
+                }
+                else
+                {
+                    textBox1.Text = File.ReadLines(name).Skip(2).First() + "\r\n" + File.ReadLines(name).Skip(3).First();
+                }                          
+            }           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Equals(""))
+            {
+                label1.Text = "Выберите задание";
+            }
+            else if (comboBox1.Text.Equals("Задание 1") && saveFileDialog1.ShowDialog() == DialogResult.OK)    //этот метод вызывает окно диалога и возвращает значение, когда нажата ОК
+            {
+                name = saveFileDialog1.FileName;
+                File.WriteAllText(name, comboBox1.Text + "\r\n" + "\r\n" + textBox1.Text + "\r\n" + textBox2.Text); //метод file пишет
+            }
+            else if (comboBox1.Text.Equals("Задание 2") && saveFileDialog1.ShowDialog() == DialogResult.OK)    //этот метод вызывает окно диалога и возвращает значение, когда нажата ОК
+            {
+                name = saveFileDialog1.FileName;
+                File.WriteAllText(name, comboBox1.Text + "\r\n" + "\r\n" + textBox1.Text); //метод file пишет
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Equals("Задание 1"))
+            {
+                label1.Text = "";
+                textBox2.Text = "";
+                if (textBox1.Text == "")
+                {
+                    label1.Text = "Введите условие для Задания 1";
+                }
+                else
+                {
+                    try
+                    {
+                        string s1 = textBox1.Lines.First();
+                        string s2 = textBox1.Lines.Skip(1).First();
+
+                        string[] array1 = s1.Split(' ', ',');
+                        string[] array2 = s2.Split(' ', ',');
+                        List<string> list = new List<string>();
+
+                        for (int i = 0; i < array1.Length; i++)
+                        {
+                            for (int j = 0; j < array2.Length; j++)
+                            {
+                                if (array2[j].Contains(array1[i]))
+                                {
+                                    list.Add(array1[i] + " ");
+                                }
+                            }
+                        }
+
+                        IEnumerable<String> noduplicates = list.Distinct();
+                        list.Sort();
+                        foreach (var name in noduplicates)
+                        {
+                            textBox2.Text += name;
+                        }
+                    }
+                    catch (System.InvalidOperationException)
+                    {
+                        label1.Text = "Условие не соответствует Заданию 1";
+                    }
+
+                }
+            }
+
+            else if (comboBox1.Text.Equals("Задание 2"))
+            {
+                label1.Text = "";
+                textBox2.Text = "";
+                if (textBox1.Text == "")
+                {
+                    label1.Text = "Введите условие для Задания 2";
+                }
+                else
+                {
+                    try
+                    {
+                        number = Convert.ToInt32(textBox1.Text);
+                        int n = number;
+                        int ed, des, sot, tis;
+                        if (textBox1.Text.Length == 1)
+                        {
+                            ed = (n % 10);
+                            textBox2.Text = ed.ToString();
+                        }
+                        else if (textBox1.Text.Length == 2)
+                        {
+                            des = (n % 100) - (n % 10);
+                            ed = (n % 10);
+                            if (ed == 0)
+                            {
+                                textBox2.Text = des.ToString();
+                            }
+                            else
+                            {
+                                textBox2.Text = des.ToString() + " + " + ed.ToString();
+                            }
+                        }
+                        else if (textBox1.Text.Length == 3)
+                        {
+                            sot = (n % 1000) - (n % 100);
+                            des = (n % 100) - (n % 10);
+                            ed = (n % 10);
+                            if (ed == 0 && des == 0)
+                            {
+                                textBox2.Text = sot.ToString();
+                            }
+                            else if (des == 0)
+                            {
+                                textBox2.Text = sot.ToString() + " + " + ed.ToString();
+                            }
+                            else if (ed == 0)
+                            {
+                                textBox2.Text = sot.ToString() + " + " + des.ToString();
+                            }
+                            else
+                            {
+                                textBox2.Text = sot.ToString() + " + " + des.ToString() + " + " + ed.ToString();
+                            }
+                        }
+                        else
+                        {
+                            tis = n - (n % 1000);
+                            sot = (n % 1000) - (n % 100);
+                            des = (n % 100) - (n % 10);
+                            ed = (n % 10);
+                            if (ed == 0 && des == 0 && sot == 0)
+                            {
+                                textBox2.Text = tis.ToString();
+                            }
+                            else if (des == 0 && sot == 0)
+                            {
+                                textBox2.Text = tis.ToString() + " + " + ed.ToString();
+                            }
+                            else if (sot == 0 && ed == 0)
+                            {
+                                textBox2.Text = tis.ToString() + " + " + des.ToString();
+                            }
+                            else if (des == 0 && ed == 0)
+                            {
+                                textBox2.Text = tis.ToString() + " + " + sot.ToString();
+                            }
+                            else if (des == 0)
+                            {
+                                textBox2.Text = tis.ToString() + " + " + sot.ToString() + " + " + ed.ToString();
+                            }
+                            else if (sot == 0)
+                            {
+                                textBox2.Text = tis.ToString() + " + " + des.ToString() + " + " + ed.ToString();
+                            }
+                            else if (ed == 0)
+                            {
+                                textBox2.Text = tis.ToString() + " + " + sot.ToString() + " + " + des.ToString();
+                            }
+                            else
+                            {
+                                textBox2.Text = tis.ToString() + " + " + sot.ToString() + " + " + des.ToString() + " + " + ed.ToString();
+                            }
+                        }
+                    }
+                    catch (System.FormatException)
+                    {
+                        label1.Text = "Условие не соответствует Заданию 2";
+                        textBox2.Clear();
+                    }
+                }
+            }
+            else
+            {
+                label1.Text = "Выберите задание";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+    }
+}
